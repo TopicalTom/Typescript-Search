e71f07886253941e4a120d62d524151e5191da25
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-25 15:36:56 -0400

    Extracted reducer logic and created exports from single location

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,32 +1,10 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[];
-};
-
-interface SearchRepositoriesAction {
-    type: ActionType.SEARCH_REPOSITORIES;
-};
-
-interface SearchRepositoriesSuccessAction {
-    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-    payload: string[]
-};
-
-interface SearchRepositoriesErrorAction {
-    type: ActionType.SEARCH_REPOSITORIES_ERROR;
-    payload: string
-};
-
-type Action = 
-    | SearchRepositoriesAction 
-    | SearchRepositoriesSuccessAction 
-    | SearchRepositoriesErrorAction;
-
-enum ActionType {
-    SEARCH_REPOSITORIES = 'SEARCH_REPOSITORIES',
-    SEARCH_REPOSITORIES_SUCCESS = 'SEARCH_REPOSITORIES_SUCCESS',
-    SEARCH_REPOSITORIES_ERROR = 'SEARCH_REPOSITORIES_ERROR'
 };
 
 const reducer = (
