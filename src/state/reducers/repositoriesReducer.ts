01d52cda57bd1261ef62c27172e17144ebf006ef
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-06-25 14:48:25 -0400

    Created repositories reducer logic

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,60 @@
+interface RepositoriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[];
+};
+
+interface SearchRepositoriesAction {
+    type: ActionType.SEARCH_REPOSITORIES;
+};
+
+interface SearchRepositoriesSuccessAction {
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+    payload: string[]
+};
+
+interface SearchRepositoriesErrorAction {
+    type: ActionType.SEARCH_REPOSITORIES_ERROR;
+    payload: string
+};
+
+type Action = 
+    | SearchRepositoriesAction 
+    | SearchRepositoriesSuccessAction 
+    | SearchRepositoriesErrorAction;
+
+enum ActionType {
+    SEARCH_REPOSITORIES = 'SEARCH_REPOSITORIES',
+    SEARCH_REPOSITORIES_SUCCESS = 'SEARCH_REPOSITORIES_SUCCESS',
+    SEARCH_REPOSITORIES_ERROR = 'SEARCH_REPOSITORIES_ERROR'
+};
+
+const reducer = (
+    state: RepositoriesState, 
+    action: Action
+): RepositoriesState => {
+    switch (action.type) {
+        case ActionType.SEARCH_REPOSITORIES:
+            return { 
+                loading: true, 
+                error: null, 
+                data: []
+            };
+        case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+            return {
+                loading: false, 
+                error: null, 
+                data: action.payload
+            };
+        case ActionType.SEARCH_REPOSITORIES_ERROR:
+            return {
+                loading: false, 
+                error: action.payload, 
+                data: []
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
